180dcbfc9926787518365454f4cf6a67a677b93f
Matt Corallo <git@bluematt.me>
2025-03-02 00:04:06 +0000

    use hours lol

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def reviewer_dashboard():
 		if review.completed_at:
 			reviewers[reviewer]['completed_reviews'].append(review)
 			delta = review.completed_at - review.requested_at
-			review_duration = delta.total_seconds() / 60  # Duration in minutes
+			review_duration = delta.total_seconds() / 60 / 60
 			reviewers[reviewer]['total_duration'] += review_duration
 			reviewers[reviewer]['total_reviews'] += 1
 		else:
